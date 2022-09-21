0737d82d34091f8233df2714d5491be7a1db4613
AlecMoore <alec.moore47@yahoo.com>
2022-09-21 20:57:50 +0100

    Add project files.

---
+++ AllManx/Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+﻿using AllManx.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AllManx.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User user)
+        {
+
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AllManx/StoredProcedures/StoredProcedures.cs
@@ -0,0 +1,91 @@
+﻿using AllManx.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OrganisedMe.StoredProcedures
+{
+    public static class StoredProcedures
+    {
+
+        public static string SqlconString = "Data Source=DESKTOP-J6APBTK;Initial Catalog=AllManx;Integrated Security=True";
+        public static bool CreateUser(User user)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(SqlconString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sql_cmnd = new SqlCommand("dbo.CreateUser", sqlCon);
+                    sql_cmnd.CommandType = CommandType.StoredProcedure;
+                    sql_cmnd.Parameters.AddWithValue("@Email", user.Email);
+                    sql_cmnd.Parameters.AddWithValue("@Password", user.Password);
+                    sql_cmnd.ExecuteNonQuery();
+                    sqlCon.Close();
+
+                    return true;
+                }
+            } catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteUser(int Id)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(SqlconString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sql_cmnd = new SqlCommand("dbo.DeleteUser", sqlCon);
+                    sql_cmnd.CommandType = CommandType.StoredProcedure;
+                    sql_cmnd.Parameters.AddWithValue("@Id", Id);
+                    sql_cmnd.ExecuteNonQuery();
+                    sqlCon.Close();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static List<User> Getusers()
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+
+                using (SqlConnection sqlCon = new SqlConnection(SqlconString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sql_cmnd = new SqlCommand("dbo.GetUsers", sqlCon);
+                    sql_cmnd.CommandType = CommandType.StoredProcedure;
+                    var dataReader = sql_cmnd.ExecuteReader();
+                    //sqlCon.Close();
+
+                    List<User> users = new List<User>();
+                    while(dataReader.Read())
+                    {
+                        User t = new User();
+                        foreach (var prop in t.GetType().GetProperties())
+                        {
+                            var propType = prop.PropertyType;
+                            prop.SetValue(t, Convert.ChangeType(dataReader[prop.Name].ToString(), propType));
+                        }
+                        users.Add(t);
+                    }
+
+                    return users;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+}
